a65f11d41debd9f88a95e68d277d86b503e28d5b
MayrThomas <tmayr1099@gmail.com>
2022-07-03 14:14:21 +0200

    lazy grid setup

--- SpeedrunMobile/app/src/main/java/at/fhhagenberg/me/ada/speedrunmobile/screens/Home.kt
+++ SpeedrunMobile/app/src/main/java/at/fhhagenberg/me/ada/speedrunmobile/screens/Home.kt
@@ -1,9 +1,12 @@
 package at.fhhagenberg.me.ada.speedrunmobile.screens
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.Icon
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
@@ -12,17 +15,25 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun Home() {
-    Box(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Icon(
-            imageVector = Icons.Filled.Home,
-            contentDescription = "home",
-            tint = Color.Blue,
-            modifier = Modifier.size(150.dp)
-                .align(Alignment.Center)
-        )
+fun Home(names: List<String>) {
+    Column(modifier = Modifier.padding(bottom = 48.dp)) {
+        Text(text = "SEARCH BAR")
+        LazyVerticalGrid(
+            cells = GridCells.Fixed(2),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(items = names) { name ->
+                Box(modifier = Modifier
+                    .size(60.dp)
+                    .background(Color.Cyan)
+                    .padding(16.dp),
+                    contentAlignment = Alignment.Center) {
+                    Text(text = name)
+                }
+            }
+        }
     }
 }
