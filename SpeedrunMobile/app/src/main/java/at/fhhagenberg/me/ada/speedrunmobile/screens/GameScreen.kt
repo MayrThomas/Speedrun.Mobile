2222216fa1a62bbf259b37125e05d8dde354f3f8
Chessner <s2010237006@students.fh-hagenberg.at>
2022-07-15 21:29:28 +0200

    Spaghetti GameScreen

--- SpeedrunMobile/app/src/main/java/at/fhhagenberg/me/ada/speedrunmobile/screens/GameScreen.kt
+++ SpeedrunMobile/app/src/main/java/at/fhhagenberg/me/ada/speedrunmobile/screens/GameScreen.kt
@@ -1,17 +1,162 @@
 package at.fhhagenberg.me.ada.speedrunmobile.screens
 
-import androidx.compose.material.Text
+import android.annotation.SuppressLint
+import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import at.fhhagenberg.me.ada.speedrunmobile.R
 import at.fhhagenberg.me.ada.speedrunmobile.core.Category
 import at.fhhagenberg.me.ada.speedrunmobile.core.Game
+import at.fhhagenberg.me.ada.speedrunmobile.core.Run
+import at.fhhagenberg.me.ada.speedrunmobile.network.SpeedrunProxyFactory
+import at.fhhagenberg.me.ada.speedrunmobile.ui.theme.SpeedrunMobileTheme
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+val testCategories: List<Category> = listOf(
+    Category(id = "02qr00pk",
+        name = "Any%",
+        rules = "Any% - Reach the credits of the game. \\r\\n\\r\\n* The run stops on credits, not the final cutscene. You must skip the final cutscene before getting your final time. \\r\\n* If you use the save \\u0026 quit mechanic at any point during a run, any delay in loading your character will cause a run to get rejected.\\r\\n\\r\\n##Chinese Translation \\u4e2d\\u6587\\u7ffb\\u8bd1\\r\\nhttps://www.bilibili.com/read/cv15826478"),
+    Category(id = "9kvnee8d",
+        name = "Any% Unrestricted",
+        rules = "Any% Unrestricted - Reach the credits of the game. \\r\\n\\r\\n* The run stops on credits, not the final cutscene. You must skip the final cutscene before getting your final time. \\r\\n* Force quitting the game via Alt+F4 is allowed for executing wrong warps. \\r\\n* Zips are allowed. \\r\\n* If you use the save \\u0026 quit mechanic at any point during a run, any delay in loading your character will cause a run to get rejected.\\r\\n\\r\\n##Chinese Translation \\u4e2d\\u6587\\u7ffb\\u8bd1\\r\\nhttps://www.bilibili.com/read/cv15826478"),
+    Category(id = "mkey0882",
+        name = "Any% No Wrong Warp",
+        rules = "Any% No Wrong Warp - Reach the credits of the game. The use of the wrong warp and \\u0022Pegasus\\u0022 glitches are banned.\\r\\n\\r\\n* The run stops on credits, not the final cutscene. You must skip the final cutscene before getting your final time.\\r\\n* If you use the save \\u0026 quit mechanic at any point during a run, any delay in loading your character will cause a run to get rejected.\\r\\n\\r\\n##Chinese Translation \\u4e2d\\u6587\\u7ffb\\u8bd1\\r\\nhttps://www.bilibili.com/read/cv15826478")
+)
+
+@SuppressLint("CoroutineCreationDuringComposition", "UnrememberedMutableState")
+@Composable
+fun GameScreen(
+    gameID: String? = null,
+    currentCategoryID: String? = null,
+    openDrawer: () -> Unit,
+) {
+    val categories = remember { mutableStateListOf<Category>() }
+    val runs = remember { mutableStateListOf<Run>() }
+    val coroutineScope = rememberCoroutineScope()
+
+    coroutineScope.launch {
+        withContext(Dispatchers.IO) {
+            val proxyCats = SpeedrunProxyFactory.createProxy().getGame(gameID!!)?.categories
+            val catID =
+                if (currentCategoryID.equals("a") && proxyCats?.isNotEmpty() == true) proxyCats.first().id else currentCategoryID
+            val proxyRuns = SpeedrunProxyFactory.createProxy().getRuns(gameID, catID!!)
+
+            withContext(Dispatchers.Main) {
+                if (proxyCats != null) {
+                    categories.addAll(proxyCats)
+                }
+                if (proxyRuns != null) {
+                    runs.addAll(proxyRuns)
+                }
+            }
+        }
+    }
+
+    CategoryNavigationBar(categories = categories,
+        openDrawer = { openDrawer() }, currentCategoryID = currentCategoryID)
+
+    RunsBody(runs = runs)
+}
 
 @Composable
-fun GameScreen(game: Game) {
-    game.id?.let { Text(it) }
+fun RunsBody(runs: List<Run>) {
+    Column {
+        runs.forEach { run ->
+            run.id?.let { Text(text = it) }
+        }
+    }
 }
 
 
 @Composable
-fun CategoryNavigationBar(categories: List<Category>) {
-
+fun Drawer(
+    modifier: Modifier = Modifier,
+    game: Game?,
+    onDestinationClicked: (game: String, category: String) -> Unit,
+) {
+    Column(
+        modifier
+            .fillMaxSize()
+            .padding(start = 24.dp, top = 48.dp)
+    ) {
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_background),
+            contentDescription = "App icon"
+        )
+        game?.categories?.forEach { category ->
+            Spacer(Modifier.height(24.dp))
+            Text(
+                text = category.name!!,
+                style = MaterialTheme.typography.h4,
+                modifier = Modifier.clickable {
+                    onDestinationClicked(game.id!!, category.id!!)
+                }
+            )
+        }
+    }
 }
+
+
+@Composable
+fun CategoryNavigationBar(
+    categories: List<Category>? = null,
+    currentCategoryID: String?,
+    openDrawer: () -> Unit,
+) {
+    var catID = ""
+    if (currentCategoryID.equals("a") && categories?.isNotEmpty() == true) {
+        catID = categories.first().id.toString()
+    }
+    val categoryName = categories?.find { cat -> cat.id.equals(catID) }?.name
+    TopAppBar(
+        title = {
+            Text(
+                text = categoryName ?: "",
+                color = Color.White
+            )
+        },
+        navigationIcon = {
+            IconButton(onClick = { openDrawer() }) {
+                Icon(imageVector = Icons.Filled.Menu, null)
+            }
+        },
+
+        elevation = 12.dp
+    )
+}
+
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun CategoryNavigationBarPreviewDark() {
+    SpeedrunMobileTheme {
+        CategoryNavigationBar(categories = testCategories,
+            openDrawer = {}, currentCategoryID = "Any%")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CategoryNavigationBarPreviewLight() {
+    SpeedrunMobileTheme {
+        CategoryNavigationBar(categories = testCategories,
+            openDrawer = {}, currentCategoryID = "Any%")
+    }
+}
+
